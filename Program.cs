71c29aef39e5abc27f8c2cea540c1ac9ec20c6af
IVadim23 <inazrski@gmail.com>
2024-08-04 00:47:39 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _04._Vacation_Books_List
 {
@@ -6,12 +6,15 @@ namespace _04._Vacation_Books_List
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Add page:");
             int page = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add numbers of page per hours:");
             int pagePerHours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add nubmers of days:");
             int days = int.Parse(Console.ReadLine());
             int totalHours = page / pagePerHours;
             int hoursPerDay = totalHours / days;
-            Console.WriteLine(hoursPerDay);
+            Console.WriteLine("Numbers of hours per day \n {0:f2}",hoursPerDay);
         }
     }
 }
